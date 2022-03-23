26f2b37586d03230cb8bff2c0359872301df87fa
Theresa Dillon <97997820+UniqueAquarius@users.noreply.github.com>
2022-03-23 18:10:49 -0400

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,29 @@
+for (i - 1; i <= 7; i++){
+    console.log(i);
+};
+
+console.log('==========');
+// 
+for (i = 5; i <= 25; i += 4){
+    console.log(i);
+};
+
+console.log('==========');
+// 
+const wizards = [
+    'Harry Potter',
+    'Hermione Granger',
+    'Ron Weasley'
+];
+
+for (list of wizards) {
+    console.log(list);
+};
+
+console.log('==========');
+// 
+let harryPotterMovies = 0;
+
+while (harryPotterMovies < 9){
+    console.log(harryPotterMovies);
+    harryPotterMovies++;
